009f85371af48e315846f8b01039e9f97e0f2843
HamiltonMassango <hamiltonsilva551@gmail.com>
2019-10-16 08:48:17 +0200

    Ok ! ^_^

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+
+
+let porta = '8080';
+
+
+
+server.get('/', (req, res ) => res.json({'nome' : 'Hamilton Silva'}));
+
+server.listen(porta, () => console.log('O servidor está on-line !'));
